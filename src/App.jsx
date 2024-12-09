e176661e71d1331b3f2d0cf6e31fca366c030185
dani-agudelo <daniiagudelom@gmail.com>
2024-12-09 11:34:38 -0500

    complementando algunos pasos

--- src/App.jsx
+++ src/App.jsx
@@ -868,16 +868,14 @@ const App = () => {
 
       //? FI - Captar instrucción UC (para todas)
       const pasosFI = [
+        // este no, sería solo que el pc 'cambie de color' para indicar que se está ejecutando
         {
           inicio: 'pc',
           fin: 'mar'
         },
+        // aqui empezaría lo de las flechas
         {
           inicio: 'uc',
-          fin: 'busControl'
-        },
-        {
-          inicio: 'busControl',
           fin: 'mar'
         },
         {
@@ -901,6 +899,10 @@ const App = () => {
           fin: 'ir'
         },
         {
+          inicio: 'uc',
+          fin: 'pc'
+        },
+        {
           inicio: 'pc',
           fin: 'alu'
         },
@@ -915,11 +917,14 @@ const App = () => {
         {
           inicio: 'uc',
           fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
+          fin: 'ir'
         }
       ]
 
-      const pasosFO_LOAD = []
-
+      //? podría ser una función que le entre el registro y el valor?
       const pasosWO_LOAD = [
         {
           inicio: 'ir',
@@ -933,11 +938,23 @@ const App = () => {
 
       //? MPY, ADD, SUB
  
-      //DECODE INSTRUCTION para MPY
+      //DECODE INSTRUCTION para MPY¿
       const pasosDI_MPY = [
         {
           inicio: 'uc',
           fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
+          fin: 'ir'
+        },
+        {
+          inicio: 'uc',
+          fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
+          fin: 'bancoRegistros'
         },
         {
           inicio: 'bancoRegistros',
@@ -962,6 +979,10 @@ const App = () => {
       const pasosDI_MOV = [
         {
           inicio: 'uc',
+          fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
           fin: 'ir'
         }
       ]
@@ -970,11 +991,15 @@ const App = () => {
       const pasosFO_MOV = [
         {
           inicio: 'uc',
+          fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
           fin: 'bancoRegistros'
-        }
+        },
       ]
 
-      // WRITE OUTPUT para MPY <-- va al mismo sitio
+      // WRITE OUTPUT para MOV <-- va al mismo sitio
       const pasosWO_MOV = [
         {
           inicio: 'bancoRegistros',
@@ -988,6 +1013,14 @@ const App = () => {
 
       //? MOV CON DIRECCIONAMIENTO POR MEMORIA
       const pasosCO_MOV_MEMORIA = [
+        {
+          inicio: 'uc',
+          fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
+          fin: 'memoria'
+        }, 
         {
           inicio: 'memoria',
           fin: 'busDirecciones'
@@ -1010,6 +1043,10 @@ const App = () => {
         {
           inicio: 'busDatos',
           fin: 'memoria'
+        }, 
+        {
+          inicio: 'uc',
+          fin: 'pc'
         }
       ]
 
@@ -1017,10 +1054,18 @@ const App = () => {
       const pasosDI_JMP = [
         {
           inicio: 'uc',
+          fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
           fin: 'ir'
         },
         {
           inicio: 'ir',
+          fin: 'pc'
+        },
+        {
+          inicio: 'uc',
           fin: 'pc'
         }
       ]
@@ -1029,6 +1074,10 @@ const App = () => {
       const pasosDI_INC_DEC = [
         {
           inicio: 'uc',
+          fin: 'busControl'
+        }, 
+        {
+          inicio: 'busControl',
           fin: 'ir'
         }
       ]
@@ -1058,7 +1107,74 @@ const App = () => {
         }
       ]
       
+      // ? CMP
+      //? CMP CON DIRECCIONAMIENTO POR REGISTRO
+      const pasosDI_CMP = [
+        {
+          inicio: 'uc',
+          fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
+          fin: 'ir'
+        }
+      ]
 
+      // FETCH OPERAND para CMP, se hace 2 veces
+      const pasosFO_CMP = [
+        {
+          inicio: 'uc',
+          fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
+          fin: 'bancoRegistros'
+        },
+        {
+          inicio: 'bancoRegistros',
+          fin: 'alu'
+        }
+      ]
+
+      // WRITE OUTPUT para CMP <-- se hace 1 vez, el resultado de la comparación se va a guardar
+      // en el primer registro
+      const pasosWO_CMP = [
+        {
+          inicio: 'alu',
+          fin: 'bancoRegistros'
+        },
+        {
+          inicio: 'uc',
+          fin: 'pc'
+        }
+      ]
+
+      //? CMP CON DIRECCIONAMIENTO INMEIDATO
+      // aquí se hace 1 vez el FETCH OPERAND de CMP
+      const pasosFO_CMP_INMEDIATO = [
+        {
+          inicio: 'uc',
+          fin: 'busControl'
+        },
+        {
+          inicio: 'busControl',
+          fin: 'ir'
+        },
+        {
+          inicio: 'ir',
+          fin: 'alu'
+        }
+      ]
+      const pasosWO_CMP_INMEDIATO = [
+        {
+          inicio: 'alu',
+          fin: 'bancoRegistros'
+        }, 
+        {
+          inicio: 'uc',
+          fin: 'pc'
+        }
+      ]
       
       //*VERIFICAR QUE INSTRUCCION ES
       const tipo = direccion.valor.split(" ")[0];
